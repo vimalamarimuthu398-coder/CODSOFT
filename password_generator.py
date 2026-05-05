0f0c63fbcc9a1d89724088f019d4328fd47221d6
vimalamarimuthu398-coder <vimalamarimuthu398@gmail.com>
2026-05-05 22:27:31 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========================================================== RESTART: C:/Users/acer/Documents/password.py ==========================================================
+=== Password Generator ===
+Enter password length: 8
+Generated Password:  !
+Generated Password:  !(
+Generated Password:  !(4
+Generated Password:  !(4l
+Generated Password:  !(4l"
+Generated Password:  !(4l""
+Generated Password:  !(4l""!
+Generated Password:  !(4l""!A
